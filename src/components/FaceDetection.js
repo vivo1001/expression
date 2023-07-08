322a00108e39d4c63be618b704c697ee62c761d5
Vivek H <vivitvm007@gmail.com>
2023-07-09 01:53:07 +0530

    Bug fix: Face Detection

--- src/components/FaceDetection.js
+++ src/components/FaceDetection.js
@@ -1,107 +1,120 @@
-import React, { useRef, useEffect } from 'react'
-import '../styles/FaceDetection.css'
-import * as faceapi from 'face-api.js'
+import React, { useRef, useEffect, useState } from "react";
+import "../styles/FaceDetection.css";
+import * as faceapi from "face-api.js";
 
-const FaceDetection = ({ data, available }) => {
-	const videoRef = useRef()
-	const canvasRef = useRef()
+const FaceDetection = () => {
+  const videoRef = useRef();
+  const canvasRef = useRef();
+  const expressionRef = useRef();
+  const [isLoading, setIsLoading] = useState(true);
+  const [resultExpression, setResultExpression] = useState("");
 
-	const useTiny = true
-	let initialized = false
-	const minConfidence = 0.7
-		
-	let objExpressionDescriptors = {}
+  const useTiny = true;
+  let initialized = false;
+  const minConfidence = 0.7;
 
-	useEffect(() => {
-		const loadModels = async () => {
-			const url = process.env.PUBLIC_URL + '/models'
-			await faceapi.loadTinyFaceDetectorModel(url)
-			await faceapi.loadFaceLandmarkTinyModel(url)
-			await faceapi.loadFaceExpressionModel(url)
-			await faceapi.loadFaceRecognitionModel(url)
-			console.log('Models loaded')
-		}
-		loadModels()
+  let objExpressionDescriptors = {};
 
-		getVideo()
-	}, [])
+  useEffect(() => {
+    const loadModels = async () => {
+      const url = process.env.PUBLIC_URL + "/models";
+      await faceapi.loadTinyFaceDetectorModel(url);
+      await faceapi.loadFaceLandmarkTinyModel(url);
+      await faceapi.loadFaceExpressionModel(url);
+      await faceapi.loadFaceRecognitionModel(url);
+      console.log("Models loaded");
+      setIsLoading(false);
+    };
+    loadModels();
 
-	const dimensions = {
-		width: 640,
-		height: 480,
-	}
+    getVideo();
+  }, []);
 
-	const getVideo = () => {
-		
-		navigator.mediaDevices
-			.getUserMedia({ video: dimensions })
-			.then(stream => {
-				videoRef.current.srcObject = stream
-			})
-			.catch(err => {
-				console.error('error:', err)
-			})
-	}
+  const dimensions = {
+    width: 640,
+    height: 480,
+  };
 
-	const detect = async () => {
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: dimensions })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+      })
+      .catch((err) => {
+        console.error("error:", err);
+      });
+  };
 
-		setInterval(async () => {
-			// .detectSingleFace(videoRef.current, new faceapi.getFaceDetectorOptions())
-			const detection = await faceapi
-				.detectSingleFace(videoRef.current, new faceapi.TinyFaceDetectorOptions())
-				.withFaceLandmarks(useTiny)
-				.withFaceExpressions()
-				.withFaceDescriptor()
+  const detect = async () => {
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks(useTiny)
+        .withFaceExpressions()
+        .withFaceDescriptors();
 
-			if (detection) {
-				// console.log("Entered detection section!");
-				const resizedDetections = faceapi.resizeResults(detection, dimensions)
+      if (detections.length > 0) {
+        canvasRef.current
+          .getContext("2d")
+          .clearRect(0, 0, dimensions.width, dimensions.height);
 
-				Object.keys(resizedDetections.expressions).forEach(key => {
-					if (resizedDetections.expressions[key] > minConfidence && initialized === false)
-						if (!objExpressionDescriptors.hasOwnProperty(key)) {
-							console.log('Detection:', JSON.stringify(detection))
-							console.log('Resized Detections:', key, JSON.stringify(resizedDetections.descriptor))
+        detections.forEach((detection) => {
+          const resizedDetections = faceapi.resizeResults(
+            detection,
+            dimensions
+          );
+          showDetections(resizedDetections);
+          console.log("Resize", resizedDetections);
+          setResultExpression(
+            resizedDetections.expressions.asSortedArray()[0].expression
+          );
+        });
+      } else {
+        canvasRef.current
+          .getContext("2d")
+          .clearRect(0, 0, dimensions.width, dimensions.height);
+        setResultExpression("");
+      }
+    }, 200);
+  };
 
-							// check if face expression not fulfilled yet
-							objExpressionDescriptors[key] = resizedDetections.descriptor // update fullfilled face expressions
-							// trigger event each new expression detected
+  const showDetections = (detection) => {
+    faceapi.matchDimensions(canvasRef.current, dimensions);
+    const resizedDetections = faceapi.resizeResults(detection, dimensions);
+    const minProbability = 0.05;
 
-						}
-				})
-			}
-			if (detection !== undefined) showDetections(detection)//, Present[0])
-		}, 2000)
-	}
+    const drawBox = new faceapi.draw.DrawBox(
+      resizedDetections.detection.box,
+      "lineWidth: 3"
+    );
+    drawBox.draw(canvasRef.current);
 
-	const showDetections = (detection) => {
-		faceapi.matchDimensions(canvasRef.current, dimensions)
-		const resizedDetections = faceapi.resizeResults(detection, dimensions)
-		const minProbability = 0.05
-		canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoRef.current)
-		canvasRef.current.getContext('2d').clearRect(0, 0, dimensions.width, dimensions.height)
+    // faceapi.draw.drawDetections(canvasRef.current, resizedDetections)
+    // faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections)
+    faceapi.draw.drawFaceExpressions(
+      canvasRef.current,
+      resizedDetections,
+      minProbability
+    );
+  };
 
+  return (
+    <section>
+      <div className="container">
+        <div className="detection">
+          <video autoPlay muted ref={videoRef} onPlay={detect} />
+          <canvas ref={canvasRef} />
+        </div>
+        <div className="expression">
+          <p>Resultant Expression:</p>
+          <div ref={expressionRef} className="expression-text">
+            {resultExpression}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-		const drawBox = new faceapi.draw.DrawBox(resizedDetections.detection.box, 'lineWidth: 3')
-		drawBox.draw(canvasRef.current)
-
-		faceapi.draw.drawDetections(canvasRef.current, resizedDetections)
-		faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections)
-		faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections, minProbability)
-	}
-
-
-	return (
-		<section>
-			<div className="detection">
-				<video autoPlay muted ref={videoRef} onPlay={detect} />
-				<canvas ref={canvasRef} />
-				
-			</div>
-
-		</section>
-	)
-	
-}
-
-export default FaceDetection
+export default FaceDetection;
--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -2,19 +2,16 @@ import React, { useState } from 'react'
 import FaceDetection from './FaceDetection'
 
 const MainScreen = () => {
-    const [Face] = useState({})
-	let [Available] = useState(false)	
+
   return (
     <div>
     <div className="glitch-wrapper">
         <div className="glitch" data-glitch="Expressions">Expressions</div>
     </div> 
     
-    <FaceDetection data={Face} available={Available} />
+    <FaceDetection />
     <br/>
-    <div className="footer">
-        Face and emotion recognition app created by Vivek H
-    </div>
+
 </div>
   )
 }
